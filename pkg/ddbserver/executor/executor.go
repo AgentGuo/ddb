b15c0f866a95ad3d09531df55855e7e6fbba7a54
AgentGuo <841796600@qq.com>
2022-11-19 18:35:16 +0800

    fix: fix go.sum

--- pkg/ddbserver/executor/executor.go
+++ pkg/ddbserver/executor/executor.go
@@ -1,21 +1,19 @@
 package executor
 
 import (
-	"database/sql"
 	"fmt"
 	"github.com/AgentGuo/ddb/cmd/ddbserver/config"
 	_ "github.com/go-sql-driver/mysql"
-	"time"
 )
 
 func RunExecutor(config *config.ServerConfig) {
 	fmt.Println("i am executor hello world")
-	db, err := sql.Open("mysql", "root:foobar@tcp(127.0.0.1:23306)/ddb")
-	if err != nil {
-		panic(err)
-	}
-	// See "Important settings" section.
-	db.SetConnMaxLifetime(time.Minute * 3)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
+	//db, err := sql.Open("mysql", "root:foobar@tcp(127.0.0.1:23306)/ddb")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//// See "Important settings" section.
+	//db.SetConnMaxLifetime(time.Minute * 3)
+	//db.SetMaxOpenConns(10)
+	//db.SetMaxIdleConns(10)
 }
